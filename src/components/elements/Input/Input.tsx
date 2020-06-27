52654a10b81d92513f63d6b919598629861ddc32
Alan Araujo <alan@estartandodevs.com.br>
2020-06-26 21:57:52 -0300

    Fix lint rule and remove unused

--- src/components/elements/Input/Input.tsx
+++ src/components/elements/Input/Input.tsx
@@ -1,26 +1,18 @@
 import React from "react";
+import { useFormikContext } from "formik";
 import * as S from "./InputStyled";
-import { Field, useFormikContext } from "formik";
-import { masks } from "../../../utils/masks";
-import { typeMask } from "../../../utils/masks";
+import { masks, typeMask } from "../../../utils/masks";
+
 interface InputProps {
   label: string;
   width?: string;
-  hintLabel?: string;
   type: string;
   name: string;
   mask?: typeMask;
 }
 
-const Input: React.FC<InputProps> = ({
-  label,
-  width = "100%",
-  hintLabel = "",
-  type,
-  name,
-  mask,
-}) => {
-  const { values, setFieldValue, errors } = useFormikContext();
+const Input: React.FC<InputProps> = ({ label, type, name, mask }) => {
+  const { setFieldValue } = useFormikContext();
   const handleChange = (event: Event) => {
     const target = event.target as HTMLInputElement;
     const maskedValue = masks[mask || "standard"](target?.value || "");
