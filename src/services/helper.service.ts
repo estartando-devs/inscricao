1849ec56a14019f688f39e91a7ca76f20cba45de
Alan Araujo <alan@estartandodevs.com.br>
2020-06-28 15:47:34 -0300

    Service cep

---
+++ src/services/helper.service.ts
@@ -0,0 +1,21 @@
+import { http } from "./base.service";
+
+interface IViaCep {
+  cep: string;
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  unidade: string;
+  ibge: string;
+  gia: string;
+}
+
+export const getAddressByCep = async (cep: string): Promise<IViaCep> => {
+  return new Promise((resolve) => {
+    http<IViaCep>(`https://viacep.com.br/ws/${cep}/json/`).then((response) => {
+      resolve(response.parsedBody);
+    });
+  });
+};
