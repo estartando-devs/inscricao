7764b53b88b72d3e3775ebf35de93abdf1157918
jonataspinto <jonatas.pferreira@b2wdigital.com>
2020-06-28 19:06:08 -0300

    chore: update import for elements

--- src/components/elements/index.ts
+++ src/components/elements/index.ts
@@ -2,10 +2,12 @@
 import { Button } from "./Button";
 import { Input } from "./Input";
 import { Logo } from "./Logo";
+import { TextArea } from "./TextArea";
 
 export {
   /* PLOP_INJECT_EXPORT */
   Button,
   Input,
   Logo,
+  TextArea,
 };
