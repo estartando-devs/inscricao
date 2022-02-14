657a0f97e9f126b433c84b42b28b03e1da09afcb
Ramon <ramon1234rxm@gmail.com>
2022-02-14 14:49:59 -0300

    chore: change year to util

--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 import * as S from "./HeaderStyled";
 import { Logo } from "../../elements";
+import { currentYear } from "../../../utils/currentYear";
 
-const Header = () => {
-  const year = new Date().getFullYear();
-
-  return (
-    <S.HeaderContainer>
-      <S.SideContent>
-        <Logo width="125px" />
-      </S.SideContent>
-      <S.Separator />
-      <S.TitleContainer>
-        <S.HeaderTitle>{`Inscrições ${year}`}</S.HeaderTitle>
-      </S.TitleContainer>
-    </S.HeaderContainer>
-  );
-};
+const Header = () => (
+  <S.HeaderContainer>
+    <S.SideContent>
+      <Logo width="125px" />
+    </S.SideContent>
+    <S.Separator />
+    <S.TitleContainer>
+      <S.HeaderTitle>{`Inscrições ${currentYear}`}</S.HeaderTitle>
+    </S.TitleContainer>
+  </S.HeaderContainer>
+);
 
 export default Header;
