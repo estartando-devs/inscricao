f76465519f3c15e5bf8b1d70c73a1c35b3784ca3
jonataspinto <jonatastdb_jp@hotmail.com>
2022-02-16 17:29:38 -0300

    feat: create notification context

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { Routes } from "./pages";
 import { GlobalStyles, theme } from "./styles";
+import { NotificationProvider } from "./contexts";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Routes />
+      <NotificationProvider>
+        <GlobalStyles />
+        <Routes />
+      </NotificationProvider>
     </ThemeProvider>
   );
 }
