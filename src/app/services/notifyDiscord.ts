56c6fdd1ab675b66483565227107942c484656a5
Ramon Xavier <ramon1234rxm@gmail.com>
2025-07-14 14:59:10 -0300

    fix: update Discord webhook URL in notifyDiscord service
    - Replaced the placeholder webhook URL with the actual Discord webhook URL for notifications.
    - Ensured that the notifyDiscord function can successfully send messages to the specified Discord channel.

--- src/app/services/notifyDiscord.ts
+++ src/app/services/notifyDiscord.ts
@@ -15,7 +15,7 @@ export async function notifyDiscord({
 
   try {
     await fetch(
-      `https://discordapp.com/api/webhooks/${import.meta.env.VITE_WEBHOOK_ID}/${import.meta.env.VITE_WEBHOOK_TOKEN}`,
+      `https://discordapp.com/api/webhooks/729118719737069669/CAwTGLyMSLPjftBVNw1BZlU-68Da018TD5WuGipzQ8CIGc7jj1EK1fXpY_GilG1z9vHR`,
       {
         method: "post",
         body: JSON.stringify({
